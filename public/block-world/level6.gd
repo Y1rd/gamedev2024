28b78d4a85f7fd393c2a9cd3b520700bf894f8f1
Yird <100542617+Y1rd@users.noreply.github.com>
2025-01-18 00:20:03 -0800

    hopefully fix coin bug

--- public/block-world/level6.gd
+++ public/block-world/level6.gd
@@ -3,7 +3,8 @@ var winvalue = 14
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Globalvariable.coincollected = 0
+	for n in 3:
+		Globalvariable.coincollected = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
